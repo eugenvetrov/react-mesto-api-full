0a7d201756bb3d35541e85fd5db463774d0f27af
Eugene Vetrov <evg.vetrow@yandex.ru>
2022-06-20 23:30:03 +0500

    Back authorization to headers

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,13 +2,18 @@ const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/unauthorized');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  let payload;
-  try {
-    payload = jwt.verify(token, 'some-secret-key');
-  } catch (err) {
+  const { authorization } = req.headers;
+  if (!authorization) {
     next(new Unauthorized('Необходима авторизация'));
+  } else {
+    const token = authorization.replace('Bearer ', '');
+    let payload;
+    try {
+      payload = jwt.verify(token, 'some-secret-key');
+    } catch (err) {
+      next(new Unauthorized('Необходима авторизация'));
+    }
+    req.user = payload;
+    next();
   }
-  req.user = payload;
-  next();
 };
